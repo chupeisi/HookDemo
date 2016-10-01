fd89e6a4e220981a1f829e758f28061e7d852fe5
梓烁 <zishuo.cps@alibaba-inc.com>
2016-10-01 14:17:01 +0800

    Hook First commit

---
+++ app/src/main/obj/local/armeabi/objs/test.cy/test.cy.o.d
@@ -0,0 +1,173 @@
+/Users/peisi/Others/jnitest/MyHook/app/src/main/obj/local/armeabi/objs/test.cy/test.cy.o: \
+ /Users/peisi/Others/jnitest/MyHook/app/src/main/jni/test.cy.cpp \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/peisi/Others/jnitest/MyHook/app/src/main/jni/substrate.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/dlfcn.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/signal.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ucontext.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/user.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/stat.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stat.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/endian.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/endian.h \
+ /Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/endian.h
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/peisi/Others/jnitest/MyHook/app/src/main/jni/substrate.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/dlfcn.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ucontext.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/user.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/stat.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stat.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/endian.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/endian.h:
+
+/Users/peisi/Library/Android/ndk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/endian.h:
